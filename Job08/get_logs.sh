57a84287c43022aeee96745c46eec16977ca2d09
parallels <mohamed-malek.dhib@laplateforme.io>
2023-10-05 11:36:46 +0200

    bon

--- Job08/get_logs.sh
+++ Job08/get_logs.sh
@@ -1,12 +1,12 @@
 date_actuelle=$(date "+%d-%m-%Y-%H:%M")
 
-repertoire_backup="/home/parallels/shell-exe/Jobs08/Backup"
+repertoire_backup="/home/parallels/shell-exe/Job08/Backup"
 
 nom_fichier="number_connection-$date_actuelle"
 
 nombre_connexions=$(journalctl | grep "sshd.*Accepted" | wc -l)
 
-echo "nombre_connexions" > "$repertoire_backup/$nom_fichier"
+echo "$nombre_connexions" > "$repertoire_backup/$nom_fichier"
 
 tar -cf "$repertoire_backup/$nom_fichier.tar" "$repertoire_backup/$nom_fichier" 
 
